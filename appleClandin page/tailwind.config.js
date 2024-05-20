10ab0f37ce82cdeab66f102ecb0bee2ecf0be51b
shams220 <103259015+shams220@users.noreply.github.com>
2024-05-20 07:48:13 +0530

    newone

---
+++ appleClandin page/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage:   {
+        'iphone1':'url("https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/apple-refurb-products-home-201810_FMT_WHH?wid=1472&hei=880&fmt=png-alpha&.v=1541530932900")'
+      }
+     },
+  },
+  plugins: [],
+}
+
